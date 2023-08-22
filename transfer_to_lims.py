b287f0dd957ede46f058e3c674da021c4877b97b
Tianyu Chen <tianyu.chen@jax.org>
2023-08-22 15:50:31 -0700

    transfer_to_lims.py refactored

--- transfer_to_lims.py
+++ transfer_to_lims.py
@@ -89,7 +89,7 @@ class MonitorFolder(FileSystemEventHandler):
                                     team=az_team)
                 
             except pd.errors as err3:
-                error_message = str(err1)
+                error_message = str(err3)
                 logger.error(error_message)
                 az.create_work_item(personal_access_token=access_token,
                                     type="Bug",
